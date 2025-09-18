ab8a183c11e45653f54ca774cb50354917f6474f
erlanmetaka <erlanmetaka1996@gmail.com>
2025-09-18 09:21:59 +0700

    add lanyard react bits

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
-import Link from "next/link"
+import Image from "next/image";
+import Lanyard from "./components/Lanyard/Lanyard";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
-      <h1 className="text-4xl font-bold mb-4">Hi, I’m Febby</h1>
-      <p className="text-lg text-gray-600 mb-6">
-        A passionate Fullstack Developer building web apps with Next.js & React.
-      </p>
-      <div className="flex gap-4">
-        <Link href="/about" className="text-blue-600 hover:underline">
-          About
-        </Link>
-        <Link href="/projects" className="text-blue-600 hover:underline">
-          Projects
-        </Link>
-        <Link href="/contact" className="text-blue-600 hover:underline">
-          Contact
-        </Link>
-      </div>
-    </main>
+   <div className="min-h-screen overflow-x-hidden">
+       <div className="container border mx-auto h-screen">
+           <div className="grid grid-cols-12">
+               <div className="col-span-6">A</div>
+               <div className="col-span-6">
+                   <Lanyard />
+               </div>
+           </div>
+       </div>
+   </div>
   )
 }
